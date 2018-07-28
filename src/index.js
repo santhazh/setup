2dbe02ff1933176b3c16bda59afd7b4493fb19ed
unknown <santhazh@gmail.com>
2018-07-28 20:11:18 +0530

    setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import FormContainer from "./js/components/container/FormContainer";
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <FormContainer/>,
+    document.getElementById('create-article-form')
+  );
+
---
+++ src/js/components/container/FormContainer.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import axios from "axios";
+
+
+class FormContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+    };
+
+
+    this.handleFirstnameChange = this.handleFirstnameChange.bind(this);
+    this.handleLastnameChange = this.handleLastnameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleFirstnameChange(event) {
+    this.setState({ firstName: event.target.value });
+  }
+
+  handleLastnameChange(event) {
+    this.setState({ lastName: event.target.value });
+  }
+
+
+  handleSubmit(event) {
+    const registrationData={firstName:this.state.firstName ,lastName: this.state.lastName};
+    axios.post('/user', registrationData)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+     });
+    alert('A name was submitted: ' + JSON.stringify(registrationData));
+    //to cancel form default behaviour
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="form">
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          FItrst Name:
+          <input
+           name="firstName" type="text" value={this.state.firstName} onChange={this.handleFirstnameChange} />
+        </label>
+        <label>
+          LastName:
+          <input
+           name="lastName" type="text" value={this.state.lastName} onChange={this.handleLastnameChange} />
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+      </div>
+    );
+  }
+}
+
+export default FormContainer;
